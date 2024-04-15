becec190e7ce70aec6d4571c7623b8ea23d9642d
dimk2 <dimikir14@gmail.com>
2024-04-15 18:40:29 +0300

    Create Account View initialized

--- app/src/main/java/be/kuleuven/gt/dogapp/HomeScreenActivity.java
+++ app/src/main/java/be/kuleuven/gt/dogapp/HomeScreenActivity.java
@@ -1,11 +1,6 @@
 package be.kuleuven.gt.dogapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void onBtnCreateAcc_Clicked(View Caller) {
-
-
-        Intent intent = new Intent(this, CreateAccountActivity.class);
-        startActivity(intent);
-
-
-    }
-
 }
