b5c9c9f949ff67e6d6a2d6f871af8e373f45605c
dimk2 <dimikir14@gmail.com>
2024-04-16 15:51:28 +0200

    We can create accounts and insert them in the database - next step: login

--- app/src/main/java/be/kuleuven/gt/dogapp/CreateAccountActivity.java
+++ app/src/main/java/be/kuleuven/gt/dogapp/CreateAccountActivity.java
@@ -1,5 +1,6 @@
 package be.kuleuven.gt.dogapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import be.kuleuven.gt.dogapp.model.User;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -31,20 +44,81 @@ public class CreateAccountActivity extends AppCompatActivity {
         EditText username = (EditText) findViewById(R.id.usernameCreate);
         EditText password1 = (EditText) findViewById(R.id.passwordCreate1);
         EditText password2 = (EditText) findViewById(R.id.passwordCreate2);
+
+        String e = email.getText().toString();
+        String u = username.getText().toString();
+        String p1 = password1.getText().toString();
+        String p2 = password2.getText().toString();
         String urlCreate = "https://studev.groept.be/api/a23PT106/create_account/";
 
-        if(!password1.getText().toString().equals(password2.getText().toString()))
-        {
-            Toast.makeText(this, "The passwords do not match! Please retype.", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            Toast.makeText(this, "Account created! Back to the loading screen!", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
 
+
+        if(!(e.isEmpty()) && !(u.isEmpty())&& !(p1.isEmpty()) && !(p2.isEmpty()) ) {
+
+            if (!password1.getText().toString().equals(password2.getText().toString())) {
+                Toast.makeText(this, "The passwords do not match! Please retype.", Toast.LENGTH_SHORT).show();
+            } else {
+
+                //User user = new User(u,e,p1);
+                ProgressDialog progressDialog = new ProgressDialog(CreateAccountActivity.this);
+                progressDialog.setMessage("Uploading, please wait...");
+                progressDialog.show();
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
+                StringRequest submitRequest = new StringRequest(
+                        Request.Method.POST,
+                        urlCreate,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                progressDialog.dismiss();
+                                Toast.makeText(
+                                        CreateAccountActivity.this,
+                                        "Account created! Back to the loading screen!",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                progressDialog.dismiss();
+                                Toast.makeText(
+                                        CreateAccountActivity.this,
+                                        "Unable to create account: " + error,
+                                        Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+                )
+
+                { //NOTE THIS PART: here we are passing the POST parameters to the webservice
+                    @Override
+                    protected Map<String, String> getParams() {
+                    /* Map<String, String> with key value pairs as data load */
+                        Map<String, String> params = new HashMap<>();
+                        params.put("name", u);
+                        params.put("email", e);
+                        params.put("password", p1);
+                        return params;
+                }
+                };
+
+
+                requestQueue.add(submitRequest);
+
+
+
+
+
+
+            }
+        }
+        else {
+            Toast.makeText(this, "Please fill all necessary info!", Toast.LENGTH_SHORT).show();
         }
 
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
 
 
     }
---
+++ app/src/main/java/be/kuleuven/gt/dogapp/model/User.java
@@ -0,0 +1,82 @@
+package be.kuleuven.gt.dogapp.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class User implements Parcelable {
+
+    /*
+
+    !!!!!
+    in current stage this class is not needed, may be useful later
+
+    */
+    private String username;
+    private String email;
+    private String password;
+
+
+    public User (String n, String e, String p)
+    {
+        username = n;
+        email = e;
+        password = p;
+
+    }
+    public User(Parcel in)
+    {
+        username = in.readString();
+        email = in.readString();
+        password = in.readString();
+    }
+
+    public User(JSONObject o) {
+        try {
+            username = o.getString("name");
+            email = o.getString("email");
+            password = o.getString("password");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeString(username);
+        parcel.writeString(email);
+        parcel.writeString(password);
+    }
+
+    public Map<String, String> getPostParameters() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", username);
+        params.put("email", email);
+        params.put("password", password);
+        return params;
+    }
+}
