c17ca488e58c4f7b060466f485d50d422c8a171b
jpreyesm03 <jpreyesm03@gmail.com>
2024-04-22 14:56:30 +0200

    New Picture and MyDogsActivity

---
+++ app/src/main/java/be/kuleuven/gt/dogapp/MyDogsActivity.java
@@ -0,0 +1,26 @@
+package be.kuleuven.gt.dogapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import be.kuleuven.gt.dogapp.R;
+
+public class MyDogsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_my_dogs);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
