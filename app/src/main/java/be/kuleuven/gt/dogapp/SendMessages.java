a2836eca9efc72206a61c201a6c9447bac9edeb9
dimk2 <dimikir14@gmail.com>
2024-05-06 16:37:26 +0200

    inbox message function

--- app/src/main/java/be/kuleuven/gt/dogapp/SendMessages.java
+++ app/src/main/java/be/kuleuven/gt/dogapp/SendMessages.java
@@ -31,9 +31,7 @@ import java.util.ArrayList;
 import be.kuleuven.gt.dogapp.model.User;
 
 public class SendMessages extends AppCompatActivity {
-    private User user;
-    private ArrayList<String> messageSenders;
-    private int positionOfSpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,96 +44,7 @@ public class SendMessages extends AppCompatActivity {
             return insets;
         });
 
-        user = getIntent().getParcelableExtra("user");
-    }
-
-    private void updateSpinner() {
-        Spinner spinner = findViewById(R.id.spMyDogs);
-
-
-        // Create an ArrayAdapter using the dog names ArrayList
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, messageSenders);
-
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                // `position` parameter contains the position of the selected item in the dropdown
-                // Now you can use `position` as needed
-                positionOfSpinner = position;
-                System.out.println(dogIDs.get(positionOfSpinner).toString());
-                schBreedable.setChecked(dogsBreedingState.get(positionOfSpinner));
-                int txtToChangeAge = 0;
-                String txtToChangeBreed = "";
-                if (!dogsAge.get(positionOfSpinner).equals(" ") && !dogsAge.get(positionOfSpinner).equals("_") && !(dogsAge.get(positionOfSpinner) == null)) {
-                    try {
-                        Integer.parseInt(dogsAge.get(positionOfSpinner));
-                        txtAge.setText("Age: " + dogsAge.get(positionOfSpinner));
-                    }
-                    catch (NumberFormatException e){
-                        System.out.println("Not a number!");
-                        txtAge.setText("Age is not specified.");
-                    }
-                }
-                else {
-                    txtAge.setText("Age is not specified.");
-                }
-                if (!dogsBreed.get(positionOfSpinner).equals(" ") && !dogsBreed.get(positionOfSpinner).equals("_") && !(dogsBreed.get(positionOfSpinner) == null)) {
-                    txtBreed.setText("Breed: " + dogsBreed.get(positionOfSpinner));
-                }
-                else {
-                    txtBreed.setText("Breed is not specified.");
-                }
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // Handle case when nothing is selected (optional)
-            }
-        });
     }
 
 
-    private void getMessage(User u) {
-        String baseUrl = "https://studev.groept.be/api/a23PT106/message_senders"; // Assuming this endpoint provides message sender emails
-        String url = baseUrl + "/" + u.getIdUser();
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
-                Request.Method.GET,
-                url,
-                null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        messageSenders.clear(); // Clear previous data
-                        for (int i = 0; i < response.length(); i++) {
-                            try {
-                                JSONObject jsonObject = response.getJSONObject(i);
-                                String senderEmail = jsonObject.getString("sender_email");
-                                messageSenders.add(senderEmail);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        updateSpinner();
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(SendMessages.this, "An error occurred. Please check your network connection.", Toast.LENGTH_SHORT).show();
-                    }
-                }
-        );
-        requestQueue.add(jsonArrayRequest);
-    }
 }
